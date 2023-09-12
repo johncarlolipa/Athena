4ea6c9949eb54410bfadfc11ad6e7418aec18b8a
unknown <johncarlolipa@gmail.com>
2023-09-12 10:44:17 +0800

    added header

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,20 @@ import Image from 'next/image'
 import { Montserrat } from 'next/font/google'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
+import Head from 'next/head'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Athena E-commerce</title>
+    </Head>
+    
     <div className={`${montserrat.className}`}>
       Heloo
     </div>
+    </>
   )
 }
