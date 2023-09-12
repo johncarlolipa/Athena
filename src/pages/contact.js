8d1a2e7fe7915e921749eda7e46f20b0bd073165
unknown <johncarlolipa@gmail.com>
2023-09-12 17:39:21 +0800

    edited contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,24 +15,55 @@ const contact = () => {
             className="h-[100vh]"
           />
         </div>
-        <div class="p-4">
+        <div class="p-4 mt-6">
           <div>
             <h1 className="text-4xl font-bold">Get in touch</h1>
-            <p className="text-lg font-semibold text-gray-600">
-              dfdsggdg fgdhfdhf ffhfghfdhf fhfghfgf dfgfhdfh dgdgg dfgfh
-              dfgfdhfcfgfg
+            <p className="text-lg font-semibold text-gray-600 mt-5">
+              Do you have any queries regarding the products? Just kindly send
+              us a message and we will get in touch with you
             </p>
           </div>
-          <form className="">
-            <div className="">
-              <input type="" name="" value="" placeholder="Name" className="p-4"/>
-              <input type="" name="" value="" placeholder="Lastname" className="p-4"/>
+          <form className="mt-5 space-y-4">
+            <div className="space-x-4">
+              <input
+                type=""
+                name=""
+                value=""
+                placeholder="Name"
+                className="p-4 border border-black"
+              />
+              <input
+                type=""
+                name=""
+                value=""
+                placeholder="Lastname"
+                className="p-4 border border-black"
+              />
             </div>
-            <div className="grid">
-              <input type="" name="" value="" placeholder="Email Address" className="p-4" />
-              <input type="" name="" value="" placeholder="Message" />
+            <div className="grid space-y-4">
+              <input
+                type=""
+                name=""
+                value=""
+                placeholder="Email Address"
+                className="p-4 border border-black"
+              />
+              <textarea
+                rows="8"
+                cols=""
+                className="p-4 border border-black"
+                placeholder="Message"
+              ></textarea>
             </div>
           </form>
+          <div className="flex justify-center items-center ">
+            <button
+              type=""
+              className="p-4 border border-black text-white bg-black mt-4"
+            >
+              Submit
+            </button>
+          </div>
         </div>
       </div>
     </div>
