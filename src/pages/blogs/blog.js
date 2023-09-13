48bbb018a74b630590a2ce49ff3a7f6ac68a9e78
unknown <johncarlolipa@gmail.com>
2023-09-13 12:51:49 +0800

    fixing fetch error

--- src/pages/blogs/blog.js
+++ src/pages/blogs/blog.js
@@ -3,6 +3,7 @@ import BlogHero from "@/components/blog/BlogHero";
 import BlogList from "@/components/blog/BlogList";
 import BlogNewsletter from "@/components/blog/BlogNewsletter";
 import Link from "next/link";
+import BlogPost from "@/components/blog/BlogPost";
 
 export const getStaticProps = async () => {
   try {
@@ -10,7 +11,7 @@ export const getStaticProps = async () => {
     const data = await res.json();
 
     return {
-      props: { blogs: data },
+      props: { posts: data },
     };
   } catch (error) {
     console.error("Error fetching posts:", error);
@@ -20,26 +21,12 @@ export const getStaticProps = async () => {
   }
 };
 
-const Blog = ({ blogs }) => {
+const Blog = ({ posts }) => {
   return (
     <div>
       <BlogHero />
-      <div className="mt-20">
-        <div className="text-center text-black font-poppins text-6xl font-medium leading-normal">
-          Our Blog
-        </div>
-        <div className="text-gray-600 text-center font-poppins text-base font-normal leading-normal tracking-wide mt-4">
-          Find a bright idea to suit your taste with our great selection of
-          jackets, trousers, shirts, and dresses.
-        </div>
-      </div>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 mt-10">
-        {blogs.map((post) => (
-          <div key={post.id}>
-            <BlogList post={post} />
-          </div>
-        ))}
-      </div>
+
+      <BlogPost />
 
       <BlogNewsletter />
     </div>
