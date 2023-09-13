8987ba0e688e1e02034764e57eb553d0c47aa761
unknown <johncarlolipa@gmail.com>
2023-09-13 21:08:32 +0800

    quote

--- src/components/home/Quote.js
+++ src/components/home/Quote.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function Quote() {
   return (
-    <section className="bg-red-200 mt-10">
+    <section className="bg-gray-600 mt-10">
       <div className="p-32 flex flex-col items-center md:flex-row justify-center text-center md:text-left">
         <p className="text-black font-poppins text-3xl md:text-sl lg:text-2xl font-medium leading-normal">
           Very good Design, Flexible, Fast Support.
         </p>
-        <span className="text-gray-400 font-poppins text-3xl md:text-xl lg:text-2xl font-medium leading-normal ml-2">
+        <span className="text-white font-poppins text-3xl md:text-xl lg:text-2xl font-medium leading-normal ml-2">
           - Steve Jobs
         </span>
       </div>
