fd62007f55534084ab2fd4fd8aa03f84881ab230
unknown <johncarlolipa@gmail.com>
2023-09-13 21:12:17 +0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -74,7 +74,7 @@ const navigation = {
 
 export default function Example() {
   return (
-    <footer className="bg-gray-900 mt-10">
+    <footer className="bg-black mt-10">
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
         <nav
           className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
@@ -84,7 +84,7 @@ export default function Example() {
             <div key={item.name} className="pb-6">
               <a
                 href={item.href}
-                className="text-sm leading-6 text-gray-600 hover:text-gray-900"
+                className="text-sm leading-6 text-gray-300 hover:text-gray-900"
               >
                 {item.name}
               </a>
@@ -96,14 +96,14 @@ export default function Example() {
             <a
               key={item.name}
               href={item.href}
-              className="text-gray-400 hover:text-white"
+              className="text-gray-300 hover:text-white"
             >
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
             </a>
           ))}
         </div>
-        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
+        <p className="mt-10 text-center text-xs leading-5 text-gray-300">
           &copy; 2023 Athena, Inc. All rights reserved.
         </p>
       </div>
