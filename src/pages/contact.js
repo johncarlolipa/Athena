e9d691d2525361d678b7d2e59bc48358da6b87a7
unknown <johncarlolipa@gmail.com>
2023-09-14 20:22:24 +0800

    edit contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -210,6 +210,7 @@ export default function Contact() {
             <div className="mt-8 flex justify-end">
               <button
                 type="submit"
+                src="#"
                 className="rounded-md bg-black px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-900 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
                 Send message
